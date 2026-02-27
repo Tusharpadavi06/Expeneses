9371a2b69640c7f204bb0997676d5dc6595e2404
Mr.Tushar Mahendra Padavi <114561236+Tusharpadavi06@users.noreply.github.com>
2026-02-27 12:51:53 +0530

    Add Ludhiana to the constants list

--- constants.ts
+++ constants.ts
@@ -10,7 +10,9 @@ export const BRANCH_SALES_MAPPING: BranchSalesData = {
   "Ahmedabad": ["Ravindra kaushik"],
   "Bangalore": ["Murali Krishna","Balasubramanyam", "Tarachand"],
   "Tirupur": ["Ravi Barman","Alexander Pushkin", "Subramanian", "Mani Maran"],
+  "Ludhiana": ["Mahesh Chandeliya"],
   "Surat": ["Piyush Baid", "Anil Marthe", "Raghuveer Darbar", "Sailesh Pathak", "Vanraj Darbar"]
+  
 };
 
 export const CATEGORIES: string[] = ['Travel', 'Food', 'Accommodation', 'Other'];
